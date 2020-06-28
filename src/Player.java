a45b3cb6928bb95ca745d38b49e7fc8785d6a26f
Kanat228 <kanat.1999@list.ru>
2020-06-28 12:47:40 +0600

    Commit 4

---
+++ src/Player.java
@@ -0,0 +1,9 @@
+public class Player {
+    // Здесь хранится вариант числа
+    int number = 0;
+
+    public void guess() {
+        number = (int) (Math.random() * 10);
+        System.out.println("Думаю, это число " + number);
+    }
+}
